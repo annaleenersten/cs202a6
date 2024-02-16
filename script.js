8919e4e16b25b6b1bcb6bd25f496bf4df4083770
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2024-02-15 19:58:34 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function displayData() {
         // for (let i = 0; i < data.number-5; i++) {
         //     document.getElementById('column1').innerHTML += data.people[i].name + " " +  data.people[i].craft + '<br><br><br><br><br>';
         // }
-        document.getElementById('num1').innerHTML += data.people[0].name + " " +  data.people[0].craft;
-        document.getElementById('num2').innerHTML += data.people[1].name + " " +  data.people[1].craft;
-        document.getElementById('num3').innerHTML += data.people[2].name + " " +  data.people[2].craft;
-        document.getElementById('num4').innerHTML += data.people[3].name + " " +  data.people[3].craft;
-        document.getElementById('num5').innerHTML += data.people[4].name + " " +  data.people[4].craft;
-        document.getElementById('num6').innerHTML += data.people[5].name + " " +  data.people[5].craft;
-        document.getElementById('num7').innerHTML += data.people[6].name + " " +  data.people[6].craft;
+        document.getElementById('num1').innerHTML += data.people[0].name + ", " +  data.people[0].craft;
+        document.getElementById('num2').innerHTML += data.people[1].name + ", " +  data.people[1].craft;
+        document.getElementById('num3').innerHTML += data.people[2].name + ", " +  data.people[2].craft;
+        document.getElementById('num4').innerHTML += data.people[3].name + ", " +  data.people[3].craft;
+        document.getElementById('num5').innerHTML += data.people[4].name + ", " +  data.people[4].craft;
+        document.getElementById('num6').innerHTML += data.people[5].name + ", " +  data.people[5].craft;
+        document.getElementById('num7').innerHTML += data.people[6].name + ", " +  data.people[6].craft;
        
         // for(let i = 2; i<data.number-2; i++){
         //     document.getElementById('column2').innerHTML += data.people[i].name + " " +  data.people[i].craft + '<br><br><br><br><br>';
